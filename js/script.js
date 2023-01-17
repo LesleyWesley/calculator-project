bc0c12bc71c350f2af31ed6a32d964cf67211c6f
lesley_w <lrwesley92@gmail.com>
2023-01-16 18:20:23 -0600

    Created variables for interactive elements of calculator

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+//Variable for screen
+ const screenDisplay = document.querySelector(".num-display");
+
+//Variables for clear buttons
+  const clearAll = document.querySelector(".ac");
+  const clear = document.querySelector(".clear-button");
+
+//Variables for number buttons
+  const oneButton = document.querySelector(".one");
+  const twoButton = document.querySelector(".two");
+  const threeButton = document.querySelector(".three");
+  const fourButton = document.querySelector(".four");
+  const fiveButton = document.querySelector(".five");
+  const sixButton = document.querySelector(".six");
+  const sevenButton = document.querySelector(".seven");
+  const eightButton = document.querySelector(".eight");
+  const nineButton = document.querySelector(".nine");
+  const zeroButton = document.querySelector(".zero");
+
+//Variables for operator buttons
+  const addButton = document.querySelector(".plus");
+  const subtractButton = document.querySelector(".minus");
+  const multiplyButton = document.querySelector(".multiply");
+  const divideButton = document.querySelector('.divide');
+  const equalsButton = document.querySelector(".equals");
+
+//==============================================================
+
 //Basic math functions
 
 const add = function (num1, num2) {
