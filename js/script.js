47e8946a8a95b8f239580d2a0e33b577f1a3b22c
lesley_w <lrwesley92@gmail.com>
2023-01-16 19:10:57 -0600

    Added click events to all number buttons to display proper number on screen

--- js/script.js
+++ js/script.js
@@ -26,6 +26,68 @@
 
 //==============================================================
 
+//Functions and events for populating screen
+
+let displayArray = [];
+
+const updateDisplay = function () {
+  let displayString = displayArray.join("");
+  screenDisplay.innerText = `${displayString}`;
+}
+
+oneButton.addEventListener("click", function () {
+  displayArray.push("1");
+  updateDisplay();
+})
+
+twoButton.addEventListener("click", function () {
+  displayArray.push("2");
+  updateDisplay();
+})
+
+threeButton.addEventListener("click", function () {
+  displayArray.push("3");
+  updateDisplay();
+})
+
+fourButton.addEventListener("click", function () {
+  displayArray.push("4");
+  updateDisplay();
+})
+
+fiveButton.addEventListener("click", function () {
+  displayArray.push("5");
+  updateDisplay();
+})
+
+sixButton.addEventListener("click", function () {
+  displayArray.push("6");
+  updateDisplay();
+})
+
+sevenButton.addEventListener("click", function () {
+  displayArray.push("7");
+  updateDisplay();
+})
+
+eightButton.addEventListener("click", function() {
+  displayArray.push("8");
+  updateDisplay();
+})
+
+nineButton.addEventListener("click", function () {
+  displayArray.push("9");
+  updateDisplay();
+  console.log(displayArray);
+})
+
+zeroButton.addEventListener("click", function() {
+  displayArray.push("0");
+  updateDisplay();
+})
+
+//==============================================================
+
 //Basic math functions
 
 const add = function (num1, num2) {
